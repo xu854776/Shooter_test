fad8d26d5cae662d127d73a83873d99ac6197edf
854776Xu? <2540376620@qq.com>
2023-08-17 18:11:08 +0800

    This is shooter code!

---
+++ Shooter_test/Core/Inc/friction.h
@@ -0,0 +1,15 @@
+/*
+ * friction.h
+ *
+ *  Created on: Aug 17, 2023
+ *      Author: DELL
+ */
+
+#ifndef INC_FRICTION_H_
+#define INC_FRICTION_H_
+
+void sendMessage(void);
+void ChassisCtrl(void);
+
+
+#endif /* INC_FRICTION_H_ */
